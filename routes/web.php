c3ebd161ded8054f7a6eee87b67a4e3c06078867
agung <agung ramadahani94@gmail.com>
2026-02-10 13:36:55 +0700

    view

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.layouts.app');
 });
+
+
+Route::get('/guest', function () {
+    return view('guest.layouts.app');
+});
+
+
+
 ?>
