97b9e0bcfdc47c03328614426ef440256a448a30
agung <agung ramadahani94@gmail.com>
2026-02-26 12:05:44 +0700

    cretate view beranda,user,jurusan,pendaftaran

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -44,16 +44,7 @@
       
       {{-- end sidebar --}}
       <!-- Main Content -->
-      <div class="main-content">
-        <section class="section">
-          <div class="section-header">
-            <h1>Blank Page</h1>
-          </div>
-
-          <div class="section-body">
-          </div>
-        </section>
-      </div>
+      @yield('content')
       {{-- end content --}}
       {{--ini footer  --}}
       @include('admin.layouts.footer')
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('admin.layouts.app');
-});
+    return view('admin.pages.beranda.index');
+})->name('beranda');
 
+Route::get('/user',function(){
+    return view('admin.pages.user.index');
+})->name('user');
+
+Route::get('/jurusan',function(){
+    return view('admin.pages.jurusan.index');
+})->name('jurusan');
+
+Route::get('/pendaftaran',function(){
+    return view('admin.pages.pendaftaraan.index');
+})->name('pendaftaran');
 
 Route::get('/guest', function () {
     return view('guest.layouts.app');
